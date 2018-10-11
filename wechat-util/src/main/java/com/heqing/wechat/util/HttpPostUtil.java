362f05f6cf48ad1c4363198808b89deb5b2086a8
heqing <heqing@111.com.cn>
2018-10-11 18:17:27 +0800

    http请求

--- wechat-util/src/main/java/com/heqing/wechat/util/HttpPostUtil.java
+++ wechat-util/src/main/java/com/heqing/wechat/util/HttpPostUtil.java
@@ -2,10 +2,26 @@ package com.heqing.wechat.util;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
 
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 /**
  * http/https post访问工具类
@@ -16,7 +32,8 @@ public class HttpPostUtil {
 
     /**
      * 发送post请求,获取json格式的应答
-     * @param url 请求路径加参数
+     *
+     * @param url    请求路径加参数
      * @param params 请求参数对象
      * @return 服务端返回的数据
      */
@@ -26,74 +43,265 @@ public class HttpPostUtil {
 
     /**
      * 发送发送post请求请求,获取json格式的应答
-     * @param url 请求路径加参数
+     *
+     * @param url    请求路径加参数
      * @param params 请求参数
      * @return 服务端返回的数据
      */
     public static JSONObject getResponse(String url, String params) {
-        return JSONObject.parseObject(sendJsonRequest(url, params));
+        return JSONObject.parseObject(sendHttpPostJson(url, params));
     }
 
     /**
      * post请求连接获取相应数据
-     * @param url 请求链接
+     *
+     * @param url    请求链接
      * @param params 请求参数（json格式）
      * @return 上传成功后，微信服务器返回的数据
      */
     public static String sendJsonRequest(String url, String params) {
-        StringBuilder response = new StringBuilder();
-
-        OutputStreamWriter out = null;
-        BufferedReader reader = null;
-        try {
-            // 创建URL
-            URL httpUrl = new URL(url);
-            // 建立连接
-            HttpURLConnection conn = (HttpURLConnection) httpUrl.openConnection();
-            // 设定请求的方法为"POST"，默认是GET
-            conn.setRequestMethod("POST");
-            // Post 请求不能使用缓存
-            conn.setUseCaches(false);
-            // 设置是否向httpUrlConnection输出，因为这个是post请求，参数要放在http正文内，因此需要设为true, 默认情况下是false;
-            conn.setDoOutput(true);
-            // 设定传送的内容类型是json格式
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setRequestProperty("connection", "keep-alive");
-            conn.setInstanceFollowRedirects(true);
-            // 连接
-            conn.connect();
-
-            // 现在通过输出流对象构建对象输出流对象，以实现输出可序列化的对象。
-            out = new OutputStreamWriter(conn.getOutputStream());
-            // 向对象输出流写出数据，这些数据将存到内存缓冲区中
-            out.write(params);
-            // 刷新对象输出流，将任何字节都写入潜在的流中（些处为ObjectOutputStream）
-            out.flush();
-
-            // 读取输入流响应数据
-            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
-            }
-
-            // 断开连接
-            conn.disconnect();
-        } catch(IOException e) {
-            e.printStackTrace();
-        } finally {
-            try{
-                if(out!=null){
-                    out.close();
-                }
-                if(reader!=null){
-                    reader.close();
-                }
-            } catch(IOException ex){
-                ex.printStackTrace();
-            }
-        }
-        return response.toString();
+        return sendHttpPostJson(url, params);
     }
 
+
+    /** utf-8字符编码 */
+    public static final String CHARSET_UTF_8 = "utf-8";
+
+    /** HTTP内容类型。*/
+    public static final String CONTENT_TYPE_TEXT_HTML = "text/xml";
+
+     /** HTTP内容类型。相当于form表单的形式，提交数据 */
+    public static final String CONTENT_TYPE_FORM_URL = "application/x-www-form-urlencoded";
+
+     /** HTTP内容类型。相当于form表单的形式，提交数据 */
+    public static final String CONTENT_TYPE_JSON_URL = "application/json;charset=utf-8";
+
+     /** 连接管理器 */
+    private static PoolingHttpClientConnectionManager pool;
+
+     /** 请求配置 */
+    private static RequestConfig requestConfig;
+
+    /** 状态码 */
+    private static int STATUS_CODE = 300;
+
+    static {
+            // 初始化连接管理器
+            pool = new PoolingHttpClientConnectionManager();
+            // 将最大连接数增加到200，实际项目最好从配置文件中读取这个值
+            pool.setMaxTotal(200);
+            // 设置最大路由
+            pool.setDefaultMaxPerRoute(2);
+            // 根据默认超时限制初始化requestConfig
+            int socketTimeout = 10000;
+            int connectTimeout = 10000;
+            int connectionRequestTimeout = 10000;
+            requestConfig = RequestConfig.custom().setConnectionRequestTimeout(
+                    connectionRequestTimeout).setSocketTimeout(socketTimeout).setConnectTimeout(
+                    connectTimeout).build();
+        // 设置请求超时时间
+        requestConfig = RequestConfig.custom().setSocketTimeout(50000).setConnectTimeout(50000)
+                .setConnectionRequestTimeout(50000).build();
+    }
+
+    public static CloseableHttpClient getHttpClient() {
+        CloseableHttpClient httpClient = HttpClients.custom()
+                // 设置连接池管理
+                .setConnectionManager(pool)
+                // 设置请求配置
+                .setDefaultRequestConfig(requestConfig)
+                // 设置重试次数
+                .setRetryHandler(new DefaultHttpRequestRetryHandler(0, false))
+                .build();
+        return httpClient;
+    }
+
+    /**
+     * 发送Post请求
+     *
+     * @param httpPost
+     * @return
+     */
+    private static String sendHttpPost(HttpPost httpPost) {
+        CloseableHttpClient httpClient = null;
+        CloseableHttpResponse response = null;
+        // 响应内容
+        String responseContent = null;
+        try {
+            // 创建默认的httpClient实例.
+            httpClient = getHttpClient();
+            // 配置请求信息
+            httpPost.setConfig(requestConfig);
+            // 执行请求
+            response = httpClient.execute(httpPost);
+            // 得到响应实例
+            HttpEntity entity = response.getEntity();
+
+            // 判断响应状态
+            if (response.getStatusLine().getStatusCode() >= STATUS_CODE) {
+                throw new Exception(
+                        "HTTP Request is not success, Response code is " + response.getStatusLine().getStatusCode());
+            }
+
+            if (HttpStatus.SC_OK == response.getStatusLine().getStatusCode()) {
+                responseContent = EntityUtils.toString(entity, CHARSET_UTF_8);
+                EntityUtils.consume(entity);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 释放资源
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return responseContent;
+    }
+
+    /**
+     * 发送 post请求
+     *
+     * @param httpUrl 地址
+     */
+    public static String sendHttpPost(String httpUrl) {
+        // 创建httpPost
+        HttpPost httpPost = new HttpPost(httpUrl);
+        return sendHttpPost(httpPost);
+    }
+
+    /**
+     * 发送 post请求
+     *
+     * @param httpUrl 地址
+     * @param params  参数(格式:key1=value1&key2=value2)
+     */
+    public static String sendHttpPost(String httpUrl, String params) {
+        // 创建httpPost
+        HttpPost httpPost = new HttpPost(httpUrl);
+        try {
+            // 设置参数
+            if (params != null && params.trim().length() > 0) {
+                StringEntity stringEntity = new StringEntity(params, "UTF-8");
+                stringEntity.setContentType(CONTENT_TYPE_FORM_URL);
+                httpPost.setEntity(stringEntity);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sendHttpPost(httpPost);
+    }
+
+    /**
+     * 发送 post请求
+     *
+     * @param maps 参数
+     */
+    public static String sendHttpPost(String httpUrl, Map<String, String> maps) {
+        String parem = convertStringParamter(maps);
+        return sendHttpPost(httpUrl, parem);
+    }
+
+    /**
+     * 发送 post请求 发送json数据
+     *
+     * @param httpUrl    地址
+     * @param paramsJson 参数(格式 json)
+     */
+    public static String sendHttpPostJson(String httpUrl, String paramsJson) {
+        // 创建httpPost
+        HttpPost httpPost = new HttpPost(httpUrl);
+        try {
+            // 设置参数
+            if (paramsJson != null && paramsJson.trim().length() > 0) {
+                StringEntity stringEntity = new StringEntity(paramsJson, "UTF-8");
+                stringEntity.setContentType(CONTENT_TYPE_JSON_URL);
+                httpPost.setEntity(stringEntity);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sendHttpPost(httpPost);
+    }
+
+    /**
+     * 发送 post请求 发送xml数据
+     *
+     * @param httpUrl   地址
+     * @param paramsXml 参数(格式 Xml)
+     */
+    public static String sendHttpPostXml(String httpUrl, String paramsXml) {
+        // 创建httpPost
+        HttpPost httpPost = new HttpPost(httpUrl);
+        try {
+            // 设置参数
+            if (paramsXml != null && paramsXml.trim().length() > 0) {
+                StringEntity stringEntity = new StringEntity(paramsXml, "UTF-8");
+                stringEntity.setContentType(CONTENT_TYPE_TEXT_HTML);
+                httpPost.setEntity(stringEntity);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sendHttpPost(httpPost);
+    }
+
+    /**
+     * 发送 post请求（带文件）
+     *
+     * @param httpUrl   地址
+     * @param maps      参数
+     * @param fileLists 附件
+     */
+    public static String sendHttpPost(String httpUrl, Map<String, String> maps, List<File> fileLists) {
+        // 创建httpPost
+        HttpPost httpPost = new HttpPost(httpUrl);
+        MultipartEntityBuilder meBuilder = MultipartEntityBuilder.create();
+        if (maps != null) {
+            for (String key : maps.keySet()) {
+                meBuilder.addPart(key, new StringBody(maps.get(key), ContentType.TEXT_PLAIN));
+            }
+        }
+        if (fileLists != null) {
+            for (File file : fileLists) {
+                FileBody fileBody = new FileBody(file);
+                meBuilder.addPart("files", fileBody);
+            }
+        }
+        HttpEntity reqEntity = meBuilder.build();
+        httpPost.setEntity(reqEntity);
+        return sendHttpPost(httpPost);
+    }
+
+    /**
+     * 将map集合的键值对转化成：key1=value1&key2=value2 的形式
+     *
+     * @param parameterMap 需要转化的键值对集合
+     * @return 字符串
+     */
+    public static String convertStringParamter(Map parameterMap) {
+        StringBuffer parameterBuffer = new StringBuffer();
+        if (parameterMap != null) {
+            Iterator iterator = parameterMap.keySet().iterator();
+            String key = null;
+            String value = null;
+            while (iterator.hasNext()) {
+                key = (String) iterator.next();
+                if (parameterMap.get(key) != null) {
+                    value = (String) parameterMap.get(key);
+                } else {
+                    value = "";
+                }
+                parameterBuffer.append(key).append("=").append(value);
+                if (iterator.hasNext()) {
+                    parameterBuffer.append("&");
+                }
+            }
+        }
+        return parameterBuffer.toString();
+    }
 }
